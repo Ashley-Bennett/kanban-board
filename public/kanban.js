cc0ab73ebcc9ef200f946483dedbe874972f0d06
Ashley Bennett <codenation@CN-20.local>
2019-03-21 16:09:47 +0000

    can only have one text area open at a time

--- public/kanban.js
+++ public/kanban.js
@@ -6,34 +6,45 @@ var Kanban = new jKanban({
         console.log("Trigger on all items click!");
     },
     buttonClick: function (el, boardId) {
-        console.log(el);
-        console.log(boardId);
-        // create a form to enter element 
-        let formItem = document.createElement('form');
-        formItem.setAttribute("class", "itemform");
-        formItem.innerHTML = '<div class="form-group"><textarea class="form-control" rows="2" autofocus="true"></textarea></div><div class="form-group"><button type="submit" class="btn btn-primary btn-xs pull-right">Submit</button><button type="button" id="CancelBtn" class="btn btn-default btn-xs pull-right">Cancel</button><select class="color-select"><option value="None"></option><option value="red">Red</option><option value="green">Green</option><option value="blue">Blue</option><option value="pink">Pink</option></select></div>'
+        let board = document.getElementsByClassName("kanban-drag")
+        let array0 = board[0].children
+        let array1 = board[1].children
+        let array2 = board[2].children
+        let total0 = array0.length
+        let total1 = array1.length
+        let total2 = array2.length
+        if (array0[total0 - 1].className !== "itemform not-draggable" && array1[total1 - 1].className !== "itemform not-draggable" && array2[total2 - 1].className !== "itemform not-draggable") {
+            console.log(el);
+            console.log(boardId);
+            // create a form to enter element 
+            let formItem = document.createElement('form');
+            formItem.setAttribute("class", "itemform");
+            formItem.innerHTML = '<div class="form-group"><textarea class="form-control" rows="2" autofocus="true"></textarea></div><div class="form-group"><button type="submit" class="btn btn-primary btn-xs pull-right">Submit</button><button type="button" id="CancelBtn" class="btn btn-default btn-xs pull-right">Cancel</button><select class="color-select"><option value="None"></option><option value="red">Red</option><option value="green">Green</option><option value="blue">Blue</option><option value="pink">Pink</option></select></div>'
 
-        Kanban.addForm(boardId, formItem);
-        formItem.addEventListener("submit", function (e) {
-            e.preventDefault();
-            let text = e.target[0].value
-            let color = e.target[3].value
-            if (text !== "") {
-                Kanban.addElement(boardId, {
-                    "title": text,
-                    "id": text,
-                    "click": function (el) {
-                        Kanban.removeElement(el.dataset.eid)
-                    },
-                    "class": color,
-                })
-                formItem.parentNode.removeChild(formItem);
-            } else {
-                alert("You can't submit an empty form")
+            Kanban.addForm(boardId, formItem);
+            formItem.addEventListener("submit", function (e) {
+                e.preventDefault();
+                let text = e.target[0].value
+                let color = e.target[3].value
+                if (text !== "") {
+                    Kanban.addElement(boardId, {
+                        "title": text,
+                        "id": text,
+                        "click": function (el) {
+                            Kanban.removeElement(el.dataset.eid)
+                        },
+                        "class": color,
+                    })
+                    formItem.parentNode.removeChild(formItem);
+                } else {
+                    alert("You can't submit an empty form")
+                }
+            });
+            document.getElementById('CancelBtn').onclick = function () {
+                formItem.parentNode.removeChild(formItem)
             }
-        });
-        document.getElementById('CancelBtn').onclick = function () {
-            formItem.parentNode.removeChild(formItem)
+        } else {
+            alert("Finish your other item first")
         }
     },
     addItemButton: true,
